80c82365e31723ce41aacbbb62a203ffd300c6ba
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-01-21 18:11:30 +0000

    Overview sketch of program

--- main.c
+++ main.c
@@ -1,19 +1,47 @@
 #include <stdio.h>
 
-void AllocateMemory();
+#define POPULATION_SIZE 100
+#define GOAL "To be or not to be"
 
-int StartSimulation();
+struct Generation {
+    int number;
+    char *population[POPULATION_SIZE];
+    int *fitness[POPULATION_SIZE];
+};
+
+/* Function prototypes */
+struct Generation InitialisePopulation();
+int               ReachedGoal(struct Generation gen);
+struct Generation Reproduce(struct Generation parents);
 
 int main() {
-    AllocateMemory();
-    int nGenerations = StartSimulation();
-    printf("The simulation took %d generations.", nGenerations);
+    struct Generation parents = InitialisePopulation();
+    struct Generation children;
+
+    while (!ReachedGoal(parents)) {
+        children = Reproduce(parents);
+        parents = children;
+    }
+
+    printf("To get to goal \"%s\", it took %d generations.", GOAL, parents.number);
 }
 
-void AllocateMemory() {
-
+struct Generation InitialisePopulation() {
+    // Create random population of same length as goal
+    // Needs to also evaluate fitness
 }
 
-int StartSimulation() {
-    return 0;
+int ReachedGoal(struct Generation gen) {
+    // Just checks if the highest fitness individual is perfect
+}
+
+struct Generation Reproduce(struct Generation parents) {
+    // This is where the magic happens
+
+    // Evaluate fitness of parents
+    // Create new generation by choosing from most fit individuals with a higher probability
+    // Crossing over two partners and mutating
+    // Returns children
+
+    // Notes: make separate functions for: evaluating fitness, producing an individual
 }
