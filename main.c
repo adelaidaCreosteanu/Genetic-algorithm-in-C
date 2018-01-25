0c14f7fc2d7ac9a0ab9e04bec6b638a6f53c34b7
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-01-25 00:15:08 +0000

    Created initialise_population that calls random_strings to create first generation. evaluate_fitness uses a simple fitness function: number of correct characters (in correct position) reached_goal simply checks for an individual that reached the goal
    The goal is currently a defined constant. After basic algorithm is done (reproduce generation), I'll modify the program to accept an arbitrary goal.

--- main.c
+++ main.c
@@ -1,41 +1,92 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define POPULATION_SIZE 100
-#define GOAL "To be or not to be"
+#define POPULATION_SIZE 20
+#define GOAL            "To be or not to be"
+#define NUMBER_GENES    (sizeof GOAL - 1)
+#define GENE_ALPHABET   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+#define ALLELES         (sizeof GENE_ALPHABET - 1)
+
+#define TRUE 1
+#define FALSE 0
 
 struct Generation {
     int number;
-    char *population[POPULATION_SIZE];
-    int *fitness[POPULATION_SIZE];
+    char population[POPULATION_SIZE][NUMBER_GENES];
+    int fitness[POPULATION_SIZE];
 };
 
 /* Function prototypes */
-struct Generation InitialisePopulation();
-int               ReachedGoal(struct Generation gen);
-struct Generation Reproduce(struct Generation parents);
+void initialise_population(struct Generation *genZero);
+    void random_strings(char pop[][NUMBER_GENES]);
+void evaluate_fitness(char pop[][NUMBER_GENES], int fit[]);
+void print_generation(struct Generation *gen);
+int reached_goal(struct Generation *gen);
+void reproduce(struct Generation *parents, struct Generation *children);
 
 int main() {
-    struct Generation parents = InitialisePopulation();
-    struct Generation children;
+    size_t memoryNeeded = sizeof(char) * NUMBER_GENES * POPULATION_SIZE + sizeof(int) * (POPULATION_SIZE+1);
+    struct Generation *parents = malloc(memoryNeeded);
+    initialise_population(parents);
+//    struct Generation *children = malloc(memoryNeeded);
+//
+//    do {
+//        reproduce(parents, children);
+//        parents = children;
+//    } while (!reached_goal(parents));
+//
+//    printf("To get to goal \"%s\", it took %d generations.", GOAL, parents->number);
 
-    while (!ReachedGoal(parents)) {
-        children = Reproduce(parents);
-        parents = children;
-    }
-
-    printf("To get to goal \"%s\", it took %d generations.", GOAL, parents.number);
+    print_generation(parents);
 }
 
-struct Generation InitialisePopulation() {
+void initialise_population(struct Generation *genZero) {
     // Create random population of same length as goal
     // Needs to also evaluate fitness
+    genZero->number = 0;
+
+    srand(time(0));
+    random_strings(genZero->population);
+
+    evaluate_fitness(genZero->population, genZero->fitness);
 }
 
-int ReachedGoal(struct Generation gen) {
-    // Just checks if the highest fitness individual is perfect
+void random_strings(char pop[][NUMBER_GENES]) {
+    for (int i = 0; i < POPULATION_SIZE; i ++) {
+        // generate an individual
+        for (int c = 0; c < NUMBER_GENES; c ++) {
+            int selectedChar = rand() % ALLELES;
+            pop[i][c] = GENE_ALPHABET[selectedChar];
+        }
+    }
 }
 
-struct Generation Reproduce(struct Generation parents) {
+void evaluate_fitness(char pop[][NUMBER_GENES], int fit[]) {
+    for (int i = 0; i < POPULATION_SIZE; i ++) {
+        fit[i] = 0;
+
+        // calculate fitness for this individual
+        for (int c = 0; c < NUMBER_GENES; c ++) {
+            if (pop[i][c] == GOAL[c]) {
+                fit[i] ++;
+            }
+        }
+    }
+}
+
+// Just checks if any individual has perfect fitness
+int reached_goal(struct Generation *gen) {
+    for (int i = 0; i < POPULATION_SIZE; i ++) {
+        if (gen->fitness[i] == NUMBER_GENES) {
+            return TRUE;
+        }
+    }
+
+    return FALSE;
+}
+
+void reproduce(struct Generation *parents, struct Generation *children) {
     // This is where the magic happens
 
     // Evaluate fitness of parents
@@ -44,4 +95,24 @@ struct Generation Reproduce(struct Generation parents) {
     // Returns children
 
     // Notes: make separate functions for: evaluating fitness, producing an individual
+}
+
+void print_generation(struct Generation *gen) {
+    printf("Generation number: %d\nIndividuals:\n", gen->number);
+
+    for (int i = 0; i < POPULATION_SIZE; i ++) {
+        // Print individual's genes:
+        for (int c = 0; c < NUMBER_GENES; c ++) {
+            printf("%c", gen->population[i][c]);
+        }
+
+        // Print their fitness score:
+        printf("    fitness: %d\n", gen->fitness[i]);
+    }
+
+    if(reached_goal(gen)) {
+        printf("Generation reached goal.");
+    } else {
+        printf("Generation is imperfect.");
+    }
 }
