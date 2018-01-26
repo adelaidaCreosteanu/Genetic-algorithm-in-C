19c57ccbe767c01e69fba5e851dc21c1ece7cacc
Adelaida Creosteanu <adelaida.creosteanu@gmail.com>
2018-01-26 15:07:30 +0000

    struct Generation now uses a struct Organism - more elegant this way some refactoring Coded reproduce_generation and apply_mutation Split evaluate_fitness into two bits: one actually evaluates the population and the other finds the total fitness - I didn't like having one function with two purposes.
    Currently the cross over happens with a cutoff at the middle - might want to look into other strategies
    The fitness function is linear - I'm planning to make it more fancy (exponential) after I'm sure everything works...

--- main.c
+++ main.c
@@ -7,81 +7,181 @@
 #define NUMBER_GENES    (sizeof GOAL - 1)
 #define GENE_ALPHABET   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
 #define ALLELES         (sizeof GENE_ALPHABET - 1)
+#define MUTATION_RATE 0.02
 
 #define TRUE 1
 #define FALSE 0
 
-#define MUTATION_RATE 0.02
+struct Organism {
+    int fitness;
+    char genes[NUMBER_GENES];
+};
 
 struct Generation {
     int number;
-    char population[POPULATION_SIZE][NUMBER_GENES];
-    int fitness[POPULATION_SIZE];
+    struct Organism population[POPULATION_SIZE];
 };
 
 /* Function prototypes */
 void initialise_population(struct Generation *genZero);
-    void random_strings(char pop[][NUMBER_GENES]);
-void evaluate_fitness(char pop[][NUMBER_GENES], int fit[]);
+    void random_strings(struct Organism pop[]);
+void reproduce_generation(struct Generation *lastGen, struct Generation *newGen);
+    int select_parent(struct Organism pop[], int totalFitness);
+    void reproduce_two_organisms(const char *parentA, const char *parentB, char *siblingA, char *siblingB);
+    void apply_mutation(char child[]);
+void evaluate_fitness(struct Organism pop[]);
+int reached_goal(struct Organism pop[]);
 void print_generation(struct Generation *gen);
-int reached_goal(struct Generation *gen);
-void reproduce(struct Generation *parents, struct Generation *children);
-    int select_parent(struct Generation *gen, int totalFitness);
 
 int main() {
-    size_t memoryNeeded = sizeof(char) * NUMBER_GENES * POPULATION_SIZE + sizeof(int) * (POPULATION_SIZE+1);
-    struct Generation *parents = malloc(memoryNeeded);
+    struct Generation *parents = malloc(sizeof(struct Generation));
+    struct Generation *children = malloc(sizeof(struct Generation));
     initialise_population(parents);
-//    struct Generation *children = malloc(memoryNeeded);
-//
+
 //    do {
-//        reproduce(parents, children);
+//        reproduce_generation(parents, children);
 //        parents = children;
 //    } while (!reached_goal(parents));
-//
-//    printf("To get to goal \"%s\", it took %d generations.", GOAL, parents->number);
 
+    evaluate_fitness(parents->population);
     print_generation(parents);
+    reproduce_generation(parents, children);
+    print_generation(children);
+
+//    printf("To get to goal \"%s\", it took %d generations.", GOAL, parents->number);
+    free(parents);
+    free(children);
 }
 
 void initialise_population(struct Generation *genZero) {
     // Create random population of same length as goal
-    // Needs to also evaluate fitness
     genZero->number = 0;
 
+    // Seed the pseudo-random generator
     srand(time(0));
     random_strings(genZero->population);
-
-    evaluate_fitness(genZero->population, genZero->fitness);
 }
 
-void random_strings(char pop[][NUMBER_GENES]) {
-    for (int i = 0; i < POPULATION_SIZE; i ++) {
-        // generate an individual
+void random_strings(struct Organism pop[]) {
+    for (int o = 0; o < POPULATION_SIZE; o ++) {
+        // generate an organism's genes
         for (int c = 0; c < NUMBER_GENES; c ++) {
-            int selectedChar = rand() % ALLELES;
-            pop[i][c] = GENE_ALPHABET[selectedChar];
+            int selection = rand() % ALLELES;
+            pop[o].genes[c] = GENE_ALPHABET[selection];
+
+//            strncpy(&(pop[o].genes[c]), &(GENE_ALPHABET[selection]),1);
         }
     }
 }
 
-void evaluate_fitness(char pop[][NUMBER_GENES], int fit[]) {
-    for (int i = 0; i < POPULATION_SIZE; i ++) {
-        fit[i] = 0;
+void evaluate_fitness(struct Organism pop[]) {
+    int current;
+
+    for (int o = 0; o < POPULATION_SIZE; o ++) {
+        pop[o].fitness = 0;
+        current = 0;
 
         // calculate fitness for this individual
         for (int c = 0; c < NUMBER_GENES; c ++) {
-            if (pop[i][c] == GOAL[c]) {
-                fit[i] ++;
+            if (pop[o].genes[c] == GOAL[c]) {
+                current ++;
             }
+        }
+
+        pop[o].fitness = current;
+    }
+}
+
+int total_fitness(struct Organism *pop) {
+    int fit = 0;
+
+    for (int o = 0; o < POPULATION_SIZE; o ++) {
+        fit += pop[o].fitness;
+    }
+
+    return fit;
+}
+
+// This is where the magic happens
+void reproduce_generation(struct Generation *lastGen, struct Generation *newGen) {
+    newGen->number = lastGen->number + 1;
+
+    // Setting variables for the two populations for convenience
+    struct Organism *parents = lastGen->population;
+    struct Organism *children = newGen->population;
+
+    evaluate_fitness(parents);
+    int totalFitness = total_fitness(parents);
+
+    int sanityCheck = 0;
+
+    for (int i = 0; i < (float) POPULATION_SIZE/2; i ++) {
+        int pA = select_parent(parents, totalFitness);
+        int pB = select_parent(parents, totalFitness);
+
+        while (pA == pB) {
+            pB = select_parent(parents, totalFitness);
+            sanityCheck ++;
+
+            if (sanityCheck > 1000) {
+                printf("WE WENT INSANE!");
+                print_generation(lastGen);
+                print_generation(newGen);
+                exit(0);
+            }
+        }
+
+        // Produce two children at the two ends of the population array
+        int evilTwin = POPULATION_SIZE - i - 1;
+        reproduce_two_organisms(parents[pA].genes, parents[pB].genes, children[i].genes, children[evilTwin].genes);
+    }
+}
+
+// Returns index of selected parent
+int select_parent(struct Organism pop[], int totalFitness) {
+    int choice = rand() % (totalFitness + 1);
+
+    for (int parent = 0; parent < POPULATION_SIZE; parent ++) {
+        choice -= pop[parent].fitness;
+
+        if (choice <= 0) {
+            return parent;
+        }
+    }
+}
+
+// Applies crossover with a middle cutoff and applies mutation
+void reproduce_two_organisms(const char *parentA, const char *parentB, char *siblingA, char *siblingB) {
+    int cutoff = NUMBER_GENES/2;
+
+    for (int c = 0; c < cutoff; c ++) {
+        siblingA[c] = parentA[c];
+        siblingB[c] = parentB[c];
+    }
+    for (int c = cutoff; c < NUMBER_GENES; c ++) {
+        siblingA[c] = parentB[c];
+        siblingB[c] = parentA[c];
+    }
+    apply_mutation(siblingA);
+    apply_mutation(siblingB);
+}
+
+void apply_mutation(char child[]) {
+    for (int c = 0; c < NUMBER_GENES; c ++) {
+        double prob = (double) rand()/ RAND_MAX;    // between 0 and 1
+
+        if (prob < MUTATION_RATE) {
+            // We're mutating this gene!
+            int selection = rand() % ALLELES;
+            child[c] = GENE_ALPHABET[selection];
         }
     }
 }
 
 // Just checks if any individual has perfect fitness
-int reached_goal(struct Generation *gen) {
-    for (int i = 0; i < POPULATION_SIZE; i ++) {
-        if (gen->fitness[i] == NUMBER_GENES) {
+int reached_goal(struct Organism pop[]) {
+    for (int o = 0; o < POPULATION_SIZE; o ++) {
+        if (pop[o].fitness == NUMBER_GENES) {      // NEEDS TO BE MODIFIED if fitness function is changed
             return TRUE;
         }
     }
@@ -89,40 +189,19 @@ int reached_goal(struct Generation *gen) {
     return FALSE;
 }
 
-void reproduce(struct Generation *parents, struct Generation *children) {
-    // This is where the magic happens
-
-    // Evaluate fitness of parents
-    // Create new generation by choosing from most fit individuals with a higher probability
-    // Crossing over two partners and mutating
-    // Returns children
-
-    // Notes: make separate functions for: evaluating fitness, producing an individual
-}
-
-int select_parent(struct Generation *gen, int totalFitness) {
-    int choice = rand() % totalFitness + 1;
-    int individual = 0;
-
-    while (choice > 0) {
-        choice -= gen->fitness[individual];
-        individual ++;
-    }
-
-    return individual - 1;
-}
-
 void print_generation(struct Generation *gen) {
     printf("Generation number: %d\nIndividuals:\n", gen->number);
 
     for (int i = 0; i < POPULATION_SIZE; i ++) {
+        struct Organism *org = &(gen->population[i]);
+
         // Print individual's genes:
         for (int c = 0; c < NUMBER_GENES; c ++) {
-            printf("%c", gen->population[i][c]);
+            printf("%c", org->genes[c]);
         }
 
         // Print their fitness score:
-        printf("    fitness: %d\n", gen->fitness[i]);
+        printf("    fitness: %d\n", org->fitness);
     }
 
     if(reached_goal(gen)) {
